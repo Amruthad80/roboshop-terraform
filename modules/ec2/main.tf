dbc15424ccc6589838bd3200006d988ae3e9888a
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-19 07:26:35 +0530

    auto scaling

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -52,7 +52,7 @@ resource "aws_autoscaling_group" "main" {
     version = "$Latest"
   }
   tag {
-    key                 = "name"
+    key                 = "Name"
     propagate_at_launch = ture
     value               = "${var.name}-${var.env}"
   }
