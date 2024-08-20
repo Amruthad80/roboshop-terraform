87a219551f037a3f937bc1a48e51bb504f451257
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-20 18:21:15 +0530

    adding healthy check

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -140,6 +140,15 @@ resource "aws_lb_target_group" "main" {
   port     = var.allow_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
+    path                = "/health"
+    timeout             = 3
+  }
 }
 
 ##lb_listener
