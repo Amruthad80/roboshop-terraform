f5db8712baa0f2446603385fe0e7d9a04e5360a5
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-23 12:08:57 +0530

    ROBOSHOP container

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/Amruthad80/ansible-roboshop main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
+
+dnf install docker -y
+
+growpart /dev/nvme0n1 4
+lvextend -r -L +5G /dev/mapper/RootVG-varVol
+
+#pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
+#ansible-pull -i localhost, -U https://github.com/Amruthad80/ansible-roboshop main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
