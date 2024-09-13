f6aae614002e4898766a3a008d3068f40474ca70
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-09-13 08:02:03 +0530

    learn kubernetes

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -42,5 +42,15 @@ db = {
 }
 
 eks = {
+  node_groups = {
+    main-spot = {
+      max_size = 3
+      min_size = 1
+      instance_types = ["t3.medium"]
+      capacity_type = "SPOT"
+
+    }
+
+  }
 
 }
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -13,22 +13,22 @@ resource "aws_eks_cluster" "main" {
 #     bootstrap_cluster_creator_admin_permissions = true
 #   }
 }
-#
-# resource "aws_eks_node_group" "main" {
-#   for_each        = var.node_groups
-#   cluster_name    = aws_eks_cluster.main.name
-#   node_group_name = each.key
-#   node_role_arn   = aws_iam_role.eks-node.arn
-#   subnet_ids      = var.subnet_ids
-#   instance_types  = each.value["instance_types"]
-#   capacity_type   = each.value["capacity_type"]
-#
-#   scaling_config {
-#     desired_size = each.value["min_size"]
-#     max_size     = each.value["max_size"]
-#     min_size     = each.value["min_size"]
-#   }
-# }
+
+resource "aws_eks_node_group" "main" {
+  for_each        = var.node_groups
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.eks-node.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = each.value["instance_types"]
+  capacity_type   = each.value["capacity_type"]
+
+  scaling_config {
+    desired_size = each.value["min_size"]
+    max_size     = each.value["max_size"]
+    min_size     = each.value["min_size"]
+  }
+}
 #
 # resource "aws_eks_addon" "addons" {
 #   for_each                    = var.add_ons
